0bfee68c6a31133234880f1c0c337b719ad73feb
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-20 10:57:25 +0100

    structure

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,10 +1,41 @@
 import Express from "express";
-import createHttpError from "http-errors"
+import createHttpError from "http-errors";
 import multer from "multer";
-import q2m from "query-to-mongo"
-import { v2 as cloudinary } from "cloudinary"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
+import q2m from "query-to-mongo";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const usersRouter = Express.Router()
+const usersRouter = Express.Router();
 
-export default usersRouter
+usersRouter.post("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.get("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default usersRouter;
