e7a38a230413eee23f5ec74efc5d9e2038092fd2
Florian Dreyer <flodreyer@web.de>
2023-03-20 11:56:38 +0100

    exp routes should be done, untested

--- src/api/models.js
+++ src/api/models.js
@@ -1,1 +1,18 @@
 import mongoose from "mongoose"
+
+const {Schema, model} = mongoose
+
+const experienceSchema = new Schema(
+    {
+        role: {type: String, required: true},
+        company: {type: String, required: true},
+        startDate: {type: Date, required: true},
+        endDate: {type: Date},
+        description: {type: String, required: true},
+        area: {type: String, required: true},
+        image: {default: "https://picsum.photos/300/300", type: String, required: true}
+    },
+    {
+        timestamps: true
+    }
+)
