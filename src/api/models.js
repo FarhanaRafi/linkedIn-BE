36b017612e7130cf977ce8c12bfac4cb1fa7a900
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-20 11:11:25 +0100

    model done

--- src/api/models.js
+++ src/api/models.js
@@ -1,1 +1,20 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    surname: { type: String, require: true },
+    email: { type: String, require: true },
+    bio: { type: String, require: true },
+    title: { type: String, require: true },
+    area: { type: String, require: true },
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("User", usersSchema);
