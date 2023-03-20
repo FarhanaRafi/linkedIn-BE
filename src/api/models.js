7688bb6d1fae3e185e68f67d866dbe1c3923b4fa
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-20 14:28:35 +0100

    post done

--- src/api/models.js
+++ src/api/models.js
@@ -1,22 +1,25 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose;
 
 const experienceSchema = new Schema(
-    {
-        role: {type: String, required: true},
-        company: {type: String, required: true},
-        startDate: {type: Date, required: true},
-        endDate: {type: Date},
-        description: {type: String, required: true},
-        area: {type: String, required: true},
-        image: {default: "https://picsum.photos/300/300", type: String, required: true}
+  {
+    role: { type: String, required: true },
+    company: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date },
+    description: { type: String, required: true },
+    area: { type: String, required: true },
+    image: {
+      default: "https://picsum.photos/300/300",
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
-
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const userSchema = new Schema(
   {
@@ -32,11 +35,23 @@ const userSchema = new Schema(
         "https://res.cloudinary.com/dgfcfb0rr/image/upload/v1679308763/BE-DB/blogs/t4abipr33ez7pqezobfr.jpg",
       required: true,
     },
-    experiences: {default: [], type: [experienceSchema]},
+    experiences: { default: [], type: [experienceSchema] },
   },
   {
     timestamps: true,
   }
 );
 
+const postSchema = new Schema({
+  text: { type: String, required: true },
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/dgfcfb0rr/image/upload/v1679308763/BE-DB/blogs/t4abipr33ez7pqezobfr.jpg",
+    required: true,
+  },
+  user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
 export const UsersModel = model("User", userSchema);
+export const PostsModel = model("Post", postSchema);
--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,10 +1,98 @@
 import Express from "express";
-import createHttpError from "http-errors"
+import createHttpError from "http-errors";
 import multer from "multer";
-import q2m from "query-to-mongo"
-import { v2 as cloudinary } from "cloudinary"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
+import q2m from "query-to-mongo";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { PostsModel } from "../models.js";
 
-const postsRouter = Express.Router()
+const postsRouter = Express.Router();
 
-export default postsRouter
+const postUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "bw4_linkedin/posts",
+    },
+  }),
+}).single("image");
+
+postsRouter.post("/", async (req, res, next) => {
+  try {
+    const newPost = new PostsModel(req.body);
+    const { _id } = await newPost.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+postsRouter.get("/", async (req, res, next) => {
+  try {
+    const posts = await PostsModel.find().populate({
+      path: "user",
+      select: "name surname image",
+    });
+    res.send(posts);
+  } catch (error) {
+    next(error);
+  }
+});
+postsRouter.get("/:postId", async (req, res, next) => {
+  try {
+    const posts = await PostsModel.findById(req.params.postId).populate({
+      path: "user",
+      select: "name surname image",
+    });
+    if (posts) {
+      res.send(posts);
+    } else {
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+postsRouter.put("/:postId", async (req, res, next) => {
+  try {
+    const updatedPost = await PostsModel.findByIdAndUpdate(
+      req.params.postId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedPost) {
+      res.send(updatedPost);
+    } else {
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+postsRouter.delete("/:postId", async (req, res, next) => {
+  try {
+    const deletePost = await PostsModel.findByIdAndDelete(req.params.postId);
+    if (deletePost) {
+      res.send(204).send();
+    } else {
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+postsRouter.post("/:postId/image", postUploader, async (req, res, next) => {
+  try {
+    const post = await PostsModel.findById(req.params.postId);
+    post.image = req.file.path;
+    await post.save();
+    if (post) {
+      res.send({ message: "File uploaded successfully" });
+    } else {
+      next(createHttpError(404, `Post with id ${req.params.postId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default postsRouter;
